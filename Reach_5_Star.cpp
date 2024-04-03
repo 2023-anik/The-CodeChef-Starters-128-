2c81b447fdc220f23e6093b1e0b168dcb1ebe16f
Anik Sen <cse2023anik@gmail.com>
2024-04-04 01:09:37 +0600

    Add files via upload

---
+++ Reach_5_Star.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x, y;
+    cin>>x>>y;
+    if(x+y>=2000)
+        cout<<"YES"<<endl;
+    else
+        cout<<"NO"<<endl;
+    return 0;
+}
